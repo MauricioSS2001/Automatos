0966ffe83951fe708d622a98c35c9691fccf414a
MauricioSS2001 <mauriciocarvalhosantos@outlook.com>
2022-10-29 15:57:40 -0300

    Nova Regras

--- Automato/src/pacote/Principal.java
+++ Automato/src/pacote/Principal.java
@@ -6,33 +6,58 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-		// Instânciações
+		// Instanciacoes
 		Automato aut = new Automato();
 		Regras rgs = new Regras();
 		Scanner sc = new Scanner(System.in);
 		
-		//Variáveis
+		// Variaveis
 		String cadeia;
-		boolean regra = false;
-		int opcao_regra= 0;
+		boolean regra = false;    // Verificador de cadeias validas
+		int opcao_regra= 0;       // OpÃ§Ã£o do Menu
+		char dg1, dg2;            // Prmeiro e segundo digito da regra de subcadeia com dois digitos
 		
+		// Menu
 		System.out.println("=== Menu ===");
 		System.out.println("1. Regra de haver subcadeia com dois digitos");
-		System.out.print("\nDigite a regra que você deseja: ");
-		opcao_regra = sc.nextInt();
+		System.out.println("2. Regra de elementos pares");
+		System.out.print("\nDigite a regra que voce deseja: ");
+		opcao_regra = sc.nextInt();   // Selecao de opcao do Menu
 		
+		// Menu > Funcionamento
 		switch(opcao_regra) {
 		    case 1:
-		    System.out.println("\nDigite a cadeia a ser lida: ");
-		    cadeia = sc.next();
-		    regra = rgs.regra_dois_digitos(cadeia);
-		    while(regra == false) {
-		        System.out.println("\nCadeia invalida, digite a cadeia novamente: ");
-		        cadeia = sc.next();
-		        regra = rgs.regra_dois_digitos(cadeia);
-		    }    
-		    aut.estados(cadeia);
-		    break;
+    		    System.out.print("Digite o primeiro digito da subcadeia: ");
+    		    dg1 = sc.next().charAt(0);
+    		    System.out.print("Digite o segundo digito da subcadeia: ");
+                dg2 = sc.next().charAt(0);
+		    
+    		    System.out.println("\nDigite a cadeia a ser lida: ");
+    		    cadeia = sc.next();
+    		    regra = rgs.regra_dois_digitos(cadeia, dg1, dg2);
+    		    while(regra == false) {
+    		        System.out.println("\nCadeia invalida, digite a cadeia novamente: ");
+    		        cadeia = sc.next();
+    		        regra = rgs.regra_dois_digitos(cadeia, dg1, dg2);
+    		    }    
+    		    aut.estados(cadeia);
+    		    break;
+    		    
+		    case 2:
+		        System.out.print("Digite o elemento (ha numero de elementos par?): ");
+		        dg1 = sc.next().charAt(0);
+		        System.out.println("\nDigite a cadeia a ser lida: ");
+                cadeia = sc.next();
+                
+		        regra = rgs.regra_ndigitos_pares(cadeia, dg1);
+		        while(regra == false) {
+                    System.out.println("\nCadeia invalida, digite a cadeia novamente: ");
+                    cadeia = sc.next();
+                    regra = rgs.regra_ndigitos_pares(cadeia, dg1);
+                }
+		        aut.estados(cadeia);
+		        break;
+		        
 		}
 		
 		
--- Automato/src/pacote/Regras.java
+++ Automato/src/pacote/Regras.java
@@ -2,17 +2,32 @@ package pacote;
 
 public class Regras {
 
-    public boolean regra_dois_digitos(String cadeia) {
+    public boolean regra_dois_digitos(String cadeia, char dg1, char dg2) {
         char validacao;
         boolean valida = false;
         for(int x=0;x<cadeia.length()-1;x++) {
             validacao = cadeia.charAt(x);
-            if(validacao == '1' && cadeia.charAt(x+1) == '0') {
+            if(validacao == dg1 && cadeia.charAt(x+1) == dg2) {
                 valida = true;
             }
             
         }
         if(valida == true) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+    public boolean regra_ndigitos_pares(String cadeia, char dg1) {
+        int elementos = 0;
+        for(int x=0;x<cadeia.length()-1;x++) {
+            if(cadeia.charAt(x) == dg1) {
+                elementos = elementos + 1;
+            }
+        }
+        if(elementos != 0 && elementos%2 == 0) {
             return true;
         }
         else {
